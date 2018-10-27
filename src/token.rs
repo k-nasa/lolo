d18c0b868ea148f457fdccc2e734340ba50da2d4
k-nasa <htilcs1115@gmail.com>
2018-10-27 18:43:11 +0900

    Add token that is likely to be need

---
+++ src/token.rs
@@ -0,0 +1,39 @@
+struct Token {
+    tokne_type: TokenType,
+    literal: String,
+}
+
+enum TokenType {
+    ILLEGAL,
+    EOF,
+
+    IDENT,
+    INT,
+
+    ASSIGN,
+    PLUS,
+    MINUS,
+    BANG,
+    ASTERISK,
+    SLASH,
+    EQ,
+    NOTEQ,
+
+    LT,
+    GT,
+
+    COMMA,
+    SEMICOLON,
+
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    FUNCTION,
+    LET,
+    TRUE,
+    FALSE,
+    IF,
+    RETURN,
+}
