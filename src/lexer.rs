ef85c218ca39ded6c28ffb58573674e9ab27db97
k-nasa <htilcs1115@gmail.com>
2018-10-28 13:46:10 +0900

    Add lexer helper method

--- src/lexer.rs
+++ src/lexer.rs
@@ -55,6 +55,14 @@ impl Lexer {
     }
 }
 
+pub fn is_letter(ch: char) -> bool {
+    'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '|'
+}
+
+pub fn is_digit(ch: char) -> bool {
+    '0' <= ch && ch <= '9'
+}
+
 #[cfg(test)]
 mod tests {
     use super::super::token::TokenType::*;
