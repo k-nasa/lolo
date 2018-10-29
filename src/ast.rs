441ab6d71f85b2be19a5c5b9142a633e8e0c3a1a
k-nasa <htilcs1115@gmail.com>
2018-10-29 18:42:32 +0900

    Add ast only let statement

---
+++ src/ast.rs
@@ -0,0 +1,70 @@
+use super::token::Token;
+
+pub trait Node {
+    fn token_literal(&self) -> String;
+}
+
+pub trait Statement: Node {
+    fn statement_node();
+}
+
+pub trait Expression: Node {
+    fn expression_node();
+}
+
+// root ast node
+pub struct Program {
+    pub statements: Vec<Statements>,
+}
+
+impl Program {
+    fn token_literal(&self) -> String {
+        if self.statements.len() <= 0 {
+            return "".to_string();
+        }
+
+        self.statements[0].token_literal()
+    }
+}
+
+pub enum Statements {
+    LetStatement(LetStatement),
+}
+
+impl Node for Statements {
+    fn token_literal(&self) -> String {
+        match self {
+            &Statements::LetStatement(ref x) => x.token.literal.clone(),
+        }
+    }
+}
+
+pub struct LetStatement {
+    token: Token,
+    name: Identifier,
+}
+
+impl Node for LetStatement {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+}
+
+impl Statement for LetStatement {
+    fn statement_node() {}
+}
+
+pub struct Identifier {
+    token: Token,
+    value: String,
+}
+
+impl Node for Identifier {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+}
+
+impl Statement for Identifier {
+    fn statement_node() {}
+}
