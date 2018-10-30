b4be5abcda2d0986973abd69e346f27cf6bccc57
k-nasa <htilcs1115@gmail.com>
2018-10-30 18:53:53 +0900

    Move lexer test code to tests dir

---
+++ tests/lexer.rs
@@ -0,0 +1,64 @@
+extern crate lolo;
+
+#[cfg(test)]
+mod test {
+    use lolo::ast::Node;
+    use lolo::ast::Statements;
+    use lolo::lexer::Lexer;
+    use lolo::parser::Parser;
+
+    #[test]
+    fn is_should_parse_let_statement() {
+        let input = "
+            let x = 5;
+            let y = 10;
+            let hoge = 89898989;
+        ";
+
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse_program();
+
+        assert_eq!(program.statements.len(), 3);
+
+        let expect_names = ["x", "y", "hoge"];
+
+        for (i, name) in expect_names.iter().enumerate() {
+            let smtm = &program.statements[i];
+
+            test_let_statement(smtm, name);
+        }
+    }
+
+    #[test]
+    fn is_should_parse_return_statement() {
+        let input = "
+            return 5;
+            return 10;
+            return 89898989;
+        ";
+
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse_program();
+
+        assert_eq!(program.statements.len(), 3);
+
+        for stmt in program.statements {
+            assert_eq!(stmt.token_literal(), "return");
+        }
+    }
+
+    fn test_let_statement(stmt: &Statements, name: &str) {
+        assert_eq!(stmt.token_literal(), "let");
+
+        let let_stmt;
+        match stmt {
+            Statements::LetStatement(stmt) => let_stmt = stmt,
+            _ => panic!(),
+        };
+
+        assert_eq!(let_stmt.name.value, name);
+        assert_eq!(let_stmt.name.token_literal(), name);
+    }
+}
