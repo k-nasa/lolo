c7564efda073e7219486ba7d7913df2a72a61ebd
k-nasa <htilcs1115@gmail.com>
2018-11-17 18:07:02 +0900

    Add eval test code

---
+++ tests/evaluator.rs
@@ -0,0 +1,32 @@
+extern crate lolo;
+
+#[cfg(test)]
+mod test {
+    use lolo::evaluator::eval;
+    use lolo::lexer::*;
+    use lolo::object::*;
+    use lolo::parser::*;
+
+    #[test]
+    fn is_should_eval_integer_expression() {
+        let test_cases = vec![("5", 5), ("10", 10)];
+
+        for t in test_cases {
+            let evaluated = test_eval(t.0);
+            test_integer_object(evaluated, t.1);
+        }
+    }
+
+    fn test_eval(input: &str) -> Object {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse_program();
+
+        lolo::evaluator::eval(program)
+    }
+
+    fn test_integer_object(obj: Object, expected: i64) {
+        assert_eq!(ObjectType::Integer(expected), obj.object_type);
+        assert_eq!(expected.to_string(), obj.inspect());
+    }
+}
