16d5d26d9188c8d3019697fdd744d071084ebfe5
k-nasa <htilcs1115@gmail.com>
2018-11-20 21:36:49 +0900

    Add bool eval test code

--- tests/evaluator.rs
+++ tests/evaluator.rs
@@ -17,16 +17,32 @@ mod test {
         }
     }
 
+    #[test]
+    fn is_should_eval_boolean_expression() {
+        let test_cases = vec![("true", true), ("false", false)];
+
+        for t in test_cases {
+            let evaluated = test_eval(t.0);
+            test_boolean_object(evaluated, t.1);
+        }
+    }
+
+
     fn test_eval(input: &str) -> Object {
         let lexer = Lexer::new(input);
         let mut parser = Parser::new(lexer);
         let program = parser.parse_program();
 
-        lolo::evaluator::eval(program)
+        lolo::evaluator::eval(program).expect("fald eval")
     }
 
     fn test_integer_object(obj: Object, expected: i64) {
         assert_eq!(ObjectType::Integer(expected), obj.object_type);
+        assert_eq!(expected.to_string(), obj.inspect());
+    }
+
+    fn test_boolean_object(obj: Object, expected: bool) {
+        assert_eq!(ObjectType::Boolean(expected), obj.object_type);
         assert_eq!(expected.to_string(), obj.inspect());
     }
 }
